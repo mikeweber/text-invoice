060f5be7a1341e06522e1a2457a6f942bacf7823
Mike Weber <mike@weberapps.com>
2014-12-02 21:15:39 -0600

    tweak some code and alignment/visual issues

--- lib/text-invoice/totals.rb
+++ lib/text-invoice/totals.rb
@@ -1,33 +1,39 @@
 module TextInvoice
-    class Totals
-        def process(invoice)
-            # parse
-            parsed = YAML.load(invoice)
-                    
-            # total accumulator
-            total = 0
-               
-            # process line items
-            if not parsed["items"] == nil
-                for item in parsed["items"]
-                    quantity = item["quantity"]
-                    unit = item["unit"]
-                    subtotal = quantity * unit
-                    item["subtotal"] = subtotal
-                    total += subtotal
-                end
-            end
+  class Totals
+    def process(invoice)
+      # parse
+      parsed = YAML.load(invoice)
 
-            # update totals
-            parsed["total"] = total
+      # total accumulator
+      total = 0
 
-            if parsed["paid"]
-                parsed["due"] = total - parsed["paid"]
-            else
-                parsed["due"] = total
-            end
-
-            parsed.to_yaml
+      # process line items
+      if not parsed["items"] == nil
+        for item in parsed["items"]
+          quantity         = item["quantity"]
+          unit             = item["unit"]
+          subtotal         = quantity * unit
+          item["subtotal"] = as_dollar(subtotal)
+          total           += subtotal
+          item["unit"]     = as_dollar(unit)
         end
+      end
+
+      # update totals
+      parsed["total"] = as_dollar(total)
+      parsed["due"]   = as_dollar(total - parsed["paid"].to_f)
+      parsed["paid"]  = as_dollar(parsed["paid"].to_f)
+
+      parsed.to_yaml
     end
+
+    def as_dollar(num)
+      '$' + with_commas('% 9.2f' % num)
+    end
+
+    def with_commas(num)
+      new_num = num.reverse.gsub(/(\d{3})(?=\d)/, '\\1,')
+      new_num[0..-(new_num.scan(',').size + 1)].reverse
+    end
+  end
 end
